06dde41c9bb71f2ab00650897db25e66ffa1b9c2
Your Name <your@email.com>
2026-04-23 20:21:20 +0530

    REST API project

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("API is working");
+});
+
+app.get("/users", (req, res) => {
+    res.json([{ id: 1, name: "Ciel" }]);
+});
+
+app.post("/users", (req, res) => {
+    console.log(req.body); // helps us debug
+    res.json({
+        message: "User added",
+        data: req.body
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
