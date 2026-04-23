583db6dd171ca6d9a0c35f2a9d41b111c1ed1ca6
Your Name <your@email.com>
2026-04-23 21:36:14 +0530

    fix deploy

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ app.post("/users", (req, res) => {
     });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0",() => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
